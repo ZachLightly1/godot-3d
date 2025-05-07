40d2dd58b6fcdc533cc88375dc33007b2fe54351
Zach <zachlightly@gmail.com>
2025-05-06 21:12:40 -0500

    Player logic complete

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -10,19 +10,33 @@ extends CharacterBody3D
 @onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 # source: https://youtu.be/IOe1aGY6hXA?feature=shared
 
+@onready var skin := $GodetteSkin
+
 @export var base_speed := 4.0
 @export var run_speed := 6.0
+@export var defend_speed := 2.0
+
+var speed_modifier := 1.0
 
 @onready var camera := $CameraController/Camera3D
 
 var movement_input = Vector2.ZERO
+var defend := false:
+	set(value):
+		if not defend and value:
+			skin.defend(true)
+		if defend and not value:
+			skin.defend(false)
+		defend = value
+
+var weapon_active := true
 
 func _physics_process(delta: float) -> void:
-	
-	#velocity = Vector3(movement_input.x, 0, movement_input.y) * base_speed
 	move_logic(delta)
 	jump_logic(delta)
-	
+	ability_logic()
+	if Input.is_action_just_pressed("ui_accept"):
+		hit()
 	move_and_slide()
 	
 func move_logic(delta) -> void:
@@ -32,6 +46,7 @@ func move_logic(delta) -> void:
 	
 	if movement_input != Vector2.ZERO:
 		var speed := run_speed if is_running else base_speed
+		speed = defend_speed if defend else speed
 		#vel_2d += movement_input * speed * delta
 		#vel_2d = vel_2d.limit_length(speed)
 		
@@ -39,18 +54,59 @@ func move_logic(delta) -> void:
 		#velocity.z = vel_2d.y
 		
 		var desired_vel := movement_input.normalized() * speed
-		var smoothed := vel_2d.lerp(desired_vel, 0.2) # 0.2 -> 0.5 for more snap
+		var smoothed := vel_2d.lerp(desired_vel, 0.2) * speed_modifier # 0.2 -> 0.5 for more snap
 		
 		velocity.x = smoothed.x
 		velocity.z = smoothed.y
+		skin.set_move_state("Running")
+		var target_angle := -movement_input.angle() + PI/2
+		skin.rotation.y = rotate_toward(skin.rotation.y, target_angle, 6 * delta)
+		
 	else:
 		vel_2d = vel_2d.move_toward(Vector2.ZERO, base_speed * 4.0 * delta)
 		velocity.x = vel_2d.x
 		velocity.z = vel_2d.y
+		skin.set_move_state("Idle")
 	
 func jump_logic(delta: float) -> void:
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = -jump_velocity
+			do_squash_and_stretch(1.1, 0.15)
+	else:
+		skin.set_move_state("Jump")
 	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
 	velocity.y -= gravity * delta
+
+func ability_logic() -> void:
+	# actual attack
+	if Input.is_action_just_pressed("ability"):
+		if weapon_active:
+			skin.attack()
+		else:
+			skin.cast_spell()
+			stop_movement(0.3, 0.3)
+		
+	# attack
+	defend = Input.is_action_pressed("block")
+	
+	# switch weapon/magic
+	if Input.is_action_just_pressed('switch weapon') and not skin.attacking:
+		weapon_active = not weapon_active
+		skin.switch_weapon(weapon_active)
+		do_squash_and_stretch(1.1, 0.15)
+
+func stop_movement(start_duration: float, end_duration: float) -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "speed_modifier", 0.0, start_duration)
+	tween.tween_property(self, "speed_modifier", 1.0, end_duration)
+
+func hit() -> void:
+	skin.hit()
+	stop_movement(0.3, 0.9)
+
+
+func do_squash_and_stretch(value: float, duration: float = 0.1) -> void:
+	var tween = create_tween()
+	tween.tween_property(skin, "squash_and_stretch", value, duration)
+	tween.tween_property(skin, "squash_and_stretch", 1.0, duration * 1.8).set_ease(Tween.EASE_OUT)
